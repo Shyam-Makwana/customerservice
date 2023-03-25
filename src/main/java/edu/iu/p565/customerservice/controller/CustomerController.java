e6c5272355146cdb04210a27e00e23589f0deff0
Shyam-Makwana <makwanashyam6@gmail.com>
2023-03-24 20:46:13 -0400

    Added all Crud methods for customer

--- src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
+++ src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
@@ -2,17 +2,38 @@ package edu.iu.p565.customerservice.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.iu.p565.customerservice.repository.CustomerRepository;
+import org.springframework.web.bind.annotation.*;
 import edu.iu.p565.customerservice.model.Customer;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-    
+    private CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @GetMapping
     public List<Customer> findAll(){
-        return null;
+        return customerRepository.findAll();
+    }
+
+    @PostMapping
+    public int create(@Valid @RequestBody Customer customer) {
+        return customerRepository.create(customer);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Customer customer, @PathVariable int id) {
+        customerRepository.update(customer, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void update(@PathVariable int id) {
+        customerRepository.delete(id);
     }
 }
--- src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
+++ src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
@@ -9,9 +9,41 @@ import edu.iu.p565.customerservice.model.Customer;
 
 @Repository
 public class CustomerRepository {
-    private List<Customer> repository = new ArrayList<>();
+    private List<Customer> customers = new ArrayList<>();
 
     public List<Customer> findAll() {
-        return repository;
+        return customers;
+    }
+
+    public int create(Customer customer) {
+        int id = customers.size() + 1;
+        customer.setId(id);
+        customers.add(customer);
+        return id;
+    }
+
+    public void update(Customer customer, int id){
+        Customer c = getCustomerById(id);
+        if(c != null){
+            c.setName(customer.getName());
+            c.setEmail(customer.getEmail());
+        }
+        else{
+            throw new IllegalStateException("Customer with this id was not found.");
+        }
+    }
+
+    public void delete(int id){
+        Customer c = getCustomerById(id);
+        if(c != null){
+            customers.remove(c);
+        }
+        else{
+            throw new IllegalStateException("Customer with this id was not found.");
+        }
+    }
+
+    public Customer getCustomerById(int id){
+        return customers.stream().filter(c -> c.getId() == id).findAny().orElse(null);
     }
 }
